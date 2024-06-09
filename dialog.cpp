2863d3f6550ab9d838f7c2a7ac14b1a3b9ecabb3
Viktor Tsvigo <11_70@bk.ru>
2024-06-10 00:03:57 +0300

    Описание коммита

--- dialog.cpp
+++ dialog.cpp
@@ -11,6 +11,9 @@
 #include <QDebug>
 #include <memory> // Для std::unique_ptr
 #include <QProcess>
+#include <fstream>
+
+#include <iostream>
 //########################################################################################################
 // Реализация make_unique для C++11
 namespace std {
@@ -21,7 +24,7 @@ namespace std {
 }
 //########################################################################################################
 // Инициализация умного указателя на std::vector<long long>
-    std::unique_ptr<std::vector<long long>> list_of_synapses = std::make_unique<std::vector<long long>>();
+ //   std::unique_ptr<std::vector<long long>> list_of_synapses = std::make_unique<std::vector<long long>>();
      std::unique_ptr<std::vector<long long>> list_of_neurons = std::make_unique<std::vector<long long>>();
 
 //QString  Nazvaniye_fayla_s_neyronami_i_signalom="";
@@ -39,12 +42,7 @@ Dialog::Dialog(QWidget *parent)
     std::cout << "Funktsiya_Resheniya_3_uu"<< std::endl;  
 //########################################################################################################   
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// Открываем файл для решения
-// Диалог открытия файла
-//   if (Nazvaniye_fayla_s_neyronami_i_signalom=="")
-//   {
-//Nazvaniye_fayla_s_neyronami_i_signalom = QFileDialog::getOpenFileName(this,
-//              tr("Open txt"), "/home/viktor/Загрузки/data/none/300/masshtab/black-white/", tr("Txt Files (*.txt)"));
-//   }
+
 //########################################################################################################
 // читаем нейроны в вектор
  // Открытие файла для чтения
@@ -80,26 +78,7 @@ Dialog::Dialog(QWidget *parent)
     // Создание текстового потока для чтения из файла
     QTextStream in(&file);
 //###########################################################################
-//    while (!in.atEnd()) {
-//        QString line = in.readLine();
-//        bool ok;
-//        long long value = line.toLongLong(&ok);
-//        if (ok) {
-//            list_of_neurons->push_back(value);
-//        } else {
-//          qDebug() << "Не удалось преобразовать строку в число:" << line;
-//        }
-//         if (ok) {
-//   //  qDebug() << "Преобразование в long long прошло успешно:";
-//   //   qDebug() << "Значение:" << value;
-//    } else {
-//      qDebug() << "Ошибка преобразования в long long:";
-//     qDebug() << "Строка не является числовой, или значение выходит за пределы long long";
-//          chislo_oshibok_neyronov++;
-//    }
-//    }
-//    // Закрытие файла
-//    file.close();
+
 //###########################################################################
 
     // Читаем первые 201 строку из файла
@@ -135,136 +114,48 @@ Dialog::Dialog(QWidget *parent)
         std::cout << "число ошибок форматов нейронов = "<< chislo_oshibok_neyronov<< std::endl; 
     std::cout << "//########################################################################################################"<< std::endl;
 //########################################################################################################
- // Вывод содержимого вектора
-//    for (size_t i = 0; i < list_of_neurons->size(); ++i) {
-//        std::cout << "Element " << i << ": " << list_of_neurons->at(i) << std::endl;
-//    }
 //########################################################################################################
 ///#################### считываем синапсы из файла в вектор #########################################################
  // Открытие файла для чтения
- int chislo_oshibok_sinapsov=0;
    std::cout << "читаем синапсы в вектор"<< std::endl;  
-    QFile file2("/home/viktor/my_projects_qt_2/Funktsiya_Resheniya_2/synapses.txt");
-    //           /home/viktor/my_projects_qt_2/Funktsiya_Resheniya_2/synapses.txt
-    if (!file2.open(QIODevice::ReadOnly | QIODevice::Text)) {
-        qDebug() << "Не удалось открыть файл!";
-           }
-
-    // Создание текстового потока для чтения из файла
-    QTextStream in2(&file2);
+   ///////////////////////////////////// загрузка синапсов из файла в вектор
+                          std::ifstream is("/home/viktor/my_projects_qt_2/Funktsiya_Resheniya_2/synapses.txt");
+                           std::istream_iterator<unsigned long long> start(is), end;
+                           std::vector<unsigned long long> list_of_synapses(start, end);
+                           std::cout << "Read " << list_of_synapses.size() << " numbers" << std::endl;
 //###########################################################################
-//    while (!in2.atEnd()) {
-//        QString line = in2.readLine();
-//        bool ok;
-//        long long value = line.toLongLong(&ok);
-//        if (ok) {
-//            list_of_synapses->push_back(value);
-//        } else
-//        {
-////            qDebug() << "Не удалось преобразовать строку в число:" << line;
-//        }
-//         if (ok) {
-////        qDebug() << "Преобразование в long long прошло успешно:";
-////        qDebug() << "Значение:" << value;
-//    } else {
-////        qDebug() << "Ошибка преобразования в long long:";
-//      qDebug() << "Строка не является числовой, или значение выходит за пределы long long: "<< line ;
-//          chislo_oshibok_sinapsov++;
-//    }
-
-//    }
-//    // Закрытие файла
-//    file2.close();
-////###########################################################################
-
-    // Читаем первые 10105 строк из файла
-    int lineCount2 = 0;
-    while (lineCount2 < 10105 && !in2.atEnd()) {
-        QString line = in2.readLine();
-        if (line.trimmed().isEmpty()) {
-            std::cerr << "Line " << (lineCount2 + 1) << " is empty, skipping." << std::endl;
-            continue;  // Пропускаем пустые строки
-        }
-
-        bool ok;
-        long long number = line.toLongLong(&ok);
-        if (ok) {
-            list_of_neurons->push_back(number);
-            ++lineCount2;  // Увеличиваем счетчик только при успешной конвертации
-        } else {
-            std::cerr << "Failed to convert line " << (lineCount2 + 1) << " to number: " << qPrintable(line) << std::endl;
-        }
-    }
-
-    // Закрываем файл
-    file2.close();
-
-    // Проверка, что удалось прочитать ровно 201 непустую строку
-    if (lineCount2 != 10105) {
-        std::cerr << "Error: Only " << lineCount2 << " valid lines were read from the file." << std::endl;
-      //  return 1;
-    }
 /////###########################################################################
        std::cout << "конец чтения синапсов в вектор"<< std::endl;  
-         //      std::cout << "//########################################################################################################"<< std::endl;
-           std::cout << "число ошибок форматов синапсов = "<< chislo_oshibok_sinapsov<< std::endl;  
-                   std::cout << "//########################################################################################################"<< std::endl;  
+ //      std::cout << "//########################################################################################################"<< std::endl;
+
+  std::cout << "//########################################################################################################"<< std::endl;
 //########################################################################################################
-//########################################################################################################
- // Вывод содержимого вектора
-//    for (size_t i = 0; i < list_of_synapses->size(); ++i) {
-//        std::cout << "Element " << i << ": " << list_of_synapses->at(i) << std::endl;
-//    }
-//########################################################################################################
-if (chislo_oshibok_sinapsov!=0 || chislo_oshibok_neyronov!=0) // если есть ошибки в форматах синапсов или нейронов останавливаем
-// программу
-  // Остановить выполнение программы
-  {
-    exit(EXIT_SUCCESS); 
-  std::cout << "Программа остановлена. Ошибки в форматах синапсов или нейронов."<< std::endl;  
-}
+  /////////////   показываем что определила программа
+      if
+        //  ( variable_error <=0)
+          (list_of_neurons->at(200)<0)
+
+      {
+          ui->label->setText("Программа считает что это 1.");
+        std::cout << "list_of_neurons->at(200) = "<< list_of_neurons->at(200)<< std::endl;
+         // Odin_Programmi=true;
+           //  goto d;
+      }
+                             else {
+          ui->label->setText("Программа считает что это не 1.");
+          std::cout << "list_of_neurons->at(200) = "<< list_of_neurons->at(200)<< std::endl;
+     //     goto d;
+          //  Odin_Programmi=false;
+
+      }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //########################################################################################################
 //########################################################################################################             
 // блок вычисления-решения 200 нейрона
 //###########################################################################//###########################################################################
 //###########################################################################//###########################################################################
-
-//   for ( var = 100; var < 200; ++var) // This is the range of neurons
-//    {
-//        for (int neuron_index = 0, synapse_index = 0;   neuron_index < 200, synapse_index < 10100;
-//             ++neuron_index, synapse_index = synapse_index + 100)
-
-//        {
-// // list_of_synapses->at(i)
-//   list_of_neurons->at(var)
-//        =
-//        list_of_neurons->at(var)
-//        +
-//        (list_of_neurons->at(neuron_index)
-//                                                //     /
-//                           -
-//            list_of_synapses->at(synapse_index));
-
-////              });
-//        } // вычитаем синапсы
-//    }
-////########################################################################################################
-//    for (int   neuron_index = 100, synapse_index = 10000; neuron_index < 200;   ++neuron_index, ++synapse_index)
-//    {
-
-//  // list_of_neurons[200] = list_of_neurons[200] + (list_of_neurons[neuron_index] / list_of_synapses[synapse_index]); // + на -
-// list_of_neurons->at(200)
-//        =
-//        list_of_neurons->at(200)
-//        +
-//        (list_of_neurons->at(neuron_index)
-//                                           //          /
-//                  -
-//           list_of_synapses->at(synapse_index));
-//    }
-//###########################################################################//###########################################################################
-//###########################################################################//###########################################################################
 // проверка - решение
+      // если программа тут считает то 200 нейрон получается другой
 for ( var = 100;              // первый for
 var < 200
 
@@ -292,7 +183,7 @@ var < 200
                 (list_of_neurons->at(neuron_index)
           //  /   // деление
                  -
-         list_of_synapses->at(synapse_index))
+         list_of_synapses.at(synapse_index))
                     )
                     ; // + на -
 
@@ -317,7 +208,7 @@ synapse_index < 10100
             (list_of_neurons->at(neuron_index)
            //  / // деление
              -
-             list_of_synapses->at(synapse_index))
+             list_of_synapses.at(synapse_index))
                 )
     ; // + на -
 //if       (list_of_neurons->at(200)<0) goto d;
@@ -346,7 +237,7 @@ synapse_index < 10100
           Odin_Programmi=false;
     }
 //########################################################################################################
- std::cout << "list_of_neurons->at(200) = "<< list_of_neurons->at(200)<< std::endl;  
+ std::cout << "240 строка (после решения): list_of_neurons->at(200) = "<< list_of_neurons->at(200)<< std::endl;
 
 
 
